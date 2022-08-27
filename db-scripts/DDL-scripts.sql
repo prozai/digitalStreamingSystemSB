7e1ede344206625703ef9161b7c333ee7cf1958a
prozai <warriortoh@hotmail.com>
2022-08-27 23:48:45 +0800

    Upload Profile Pic to MySql Database with Hibernate Added using multipart request

--- db-scripts/DDL-scripts.sql
+++ db-scripts/DDL-scripts.sql
@@ -35,3 +35,10 @@ CREATE TABLE movie_review (
   PRIMARY KEY (review_id),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
+
+CREATE TABLE `files_upload` (
+  `upload_id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(128) DEFAULT NULL,
+  `file_data` longblob,
+  PRIMARY KEY (`upload_id`)
+)
